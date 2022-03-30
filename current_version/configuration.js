ee37f6581a54d7e1934d94b28d8c314356bed1ce
Zia Ellithiel <62364587+spacedogprincess@users.noreply.github.com>
2022-03-30 00:44:05 -0400

    Adding Elements

--- current_version/configuration.js
+++ current_version/configuration.js
@@ -15,10 +15,11 @@ var level_draw = true;
 var status_draw = true;
 var experience_draw = true;
 var moves_draw = true;
+var shiny_star_draw = true;
 
 //328 is default for sdp's code
-var health_bar_width = 322;
-var exp_bar_width = 322;
+var health_bar_width = 277;
+var exp_bar_width = 277;
 // image and sprit locations / extensions
 
 //image sprites must be lowercase nospaces of the images. e.g Max Revive = maxrevive.xxx
@@ -26,7 +27,7 @@ var item_images_root = "https://www.serebii.net/itemdex/sprites/";
 var item_extension = ".png";
 
 //pokemon sprites must be lowercase names of pokemon only. e.g. Bulbasaur = bulbasaur.xxx Mr. Mime = mr.mime.xxx Make sure the egg sprite is also with the pokemon sprites, and write its filename below as a full filename e.g. egg.png
-var pokemon_normal_sprites_root = "https://projectpokemon.org/images/normal-sprite/";
-var pokemon_shiny_sprites_root = "https://projectpokemon.org/images/shiny-sprite/";
+var pokemon_normal_sprites_root = "sprites/normal_sprites/";
+var pokemon_shiny_sprites_root = "sprites/shiny_sprites/";
 var pokemon_sprites_extension = ".gif";
 var egg_filename = "egg.png";
